49901302dcefde8e48294dcfb8bc3d8645ddb691
samueljohnraphy <77532948+samueljohnraphy@users.noreply.github.com>
2021-03-14 17:02:14 +0530

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,10 @@
+
+   class Log extends BaseClass{
+   constructor(x, y,width, angle)
+    {
+          super(x,y,width,20,angle)
+          this.image = loadImage("sprites/wood2.png");
+        }
+      
+        }
+      
